30cce7435608c95c178e1abd56902184e978c742
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-10-12 00:12:18 +0500

    added processing of cors requests

--- server/server.py
+++ server/server.py
@@ -1,12 +1,16 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 import sys
 sys.path.append('..')
 from main import replace_words_in_string, check_comands
 
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/lis/text=<text>", methods=['GET'])
+@cross_origin()
 def index(text):
 	output_text = check_comands(replace_words_in_string(text))
 	return output_text
